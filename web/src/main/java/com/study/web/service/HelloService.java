bfce90f4f69366a9b8a1a4a42fafb90682494971
LMJ <leemj6825@naver.com>
2023-05-06 03:49:34 +0900

    service 에 tx annotation 추가

--- web/src/main/java/com/study/web/service/HelloService.java
+++ web/src/main/java/com/study/web/service/HelloService.java
@@ -5,6 +5,7 @@ import com.study.web.domain.Item;
 import com.study.web.repository.HelloRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class HelloService {
 
     private final HelloRepository helloRepository;
 
+    @Transactional(readOnly = true)
     public ItemDto getItem(Long id) {
         Item item = helloRepository.getReferenceById(id);
         return ItemDto.builder()
@@ -21,6 +23,7 @@ public class HelloService {
                 .build();
     }
 
+    @Transactional
     public Long saveItem(ItemDto itemDto) {
         Item savedItem = helloRepository.save(new Item(null, itemDto.getContent(), null));
         return savedItem.getId();
